3a8a0908dd446b0bc6994bea205e62c0ddc53522
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2023-09-15 21:25:19 +0700

    Update add_disk_to_lvm.sh

--- add_disk_to_lvm.sh
+++ add_disk_to_lvm.sh
@@ -1,25 +1,22 @@
 #!/bin/bash
 
 # Get the path to the hard disk
-echo "Enter the path to the hard disk:"
-read hard_disk
+read -p "Enter the path to the hard disk: " hard_disk
 
 # Get the LVM group from the user
-echo "Enter the LVM group:"
-read LVM_group
+read -p "Enter the LVM group: " LVM_group
 
 # Get the logical volume name from the user
-echo "Enter the logical volume:"
-read logical_volume
+read -p "Enter the logical volume: " logical_volume
 
 # Reformat the hard disk for LVM
-pvcreate $hard_disk
+pvcreate "$hard_disk"
 
 # Add the hard disk to the volume group to extend the group
-vgextend $LVM_group $hard_disk
+vgextend "$LVM_group" "$hard_disk"
 
 # Extend the logical volume with the new available space
-lvextend -l +100%FREE /dev/$LVM_group/$logical_volume
+lvextend -l +100%FREE "/dev/$LVM_group/$logical_volume"
 
 # Resize the filesystem
-resize2fs /dev/$LVM_group/$logical_volume
+resize2fs "/dev/$LVM_group/$logical_volume"
