957fb3e7f009208ae953c60536a17a9be41e1f31
datthinh1801 <44528004+datthinh1801@users.noreply.github.com>
2023-09-15 21:27:52 +0700

    Update add_disk_to_lvm.sh

--- add_disk_to_lvm.sh
+++ add_disk_to_lvm.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 # Get the path to the hard disk
-read -p "Enter the path to the hard disk: " hard_disk
+read -p "Enter the path to the hard disk (e.g., /dev/sda, /dev/nvme0n1): " hard_disk
 
 # Get the LVM group from the user
-read -p "Enter the LVM group: " LVM_group
+read -p "Enter the LVM group (e.g., ubuntu-vg): " LVM_group
 
 # Get the logical volume name from the user
-read -p "Enter the logical volume: " logical_volume
+read -p "Enter the logical volume (e.g., ubuntu-lv): " logical_volume
 
 # Reformat the hard disk for LVM
 pvcreate "$hard_disk"
